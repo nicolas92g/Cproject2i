7db1053e615912849087ed7dfd248275b7763652
Nicolas GUILLOT <nicolasguillot92@gmail.com>
2024-01-15 15:25:46 +0100

    add a satellite mesh

--- MeshesLoader.c
+++ MeshesLoader.c
@@ -4,10 +4,12 @@ void MeshesLoaderCreate(MeshesLoader* self)
 {
 	MeshCreateFromObj(&self->earth, "./assets/earth/earth.obj");
 	MeshCreateFromObj(&self->ship, "./assets/starship/startshipLowPoly.obj");
+	MeshCreateFromObj(&self->satellite, "./assets/satellite/satellite.obj");
 }
 
 void MeshesLoaderDestroy(MeshesLoader* self)
 {
 	MeshDestroy(&self->earth);
 	MeshDestroy(&self->ship);
+	MeshDestroy(&self->satellite);
 }
